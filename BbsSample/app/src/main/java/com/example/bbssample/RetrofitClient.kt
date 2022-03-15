add3778b45162c0b39ce5f2cf2c6b50177d5bc0e
LEESUNHANG2 <95415549+LEESUNHANG2@users.noreply.github.com>
2022-03-15 10:23:03 +0900

    test
    test

---
+++ BbsSample/app/src/main/java/com/example/bbssample/RetrofitClient.kt
@@ -0,0 +1,48 @@
+package com.example.bbssample
+
+import android.os.StrictMode
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+class RetrofitClient {
+    companion object {
+        private var instance: Retrofit? = null
+
+        fun getInstance(): Retrofit? {
+            if(instance == null) {
+                val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+                StrictMode.setThreadPolicy(policy)
+
+                val gson = GsonBuilder()
+                    .setLenient()
+                    .create()
+
+                instance = Retrofit.Builder()
+                    .baseUrl("http://70.12.113.169:3000/")
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .addConverterFactory(ScalarsConverterFactory.create())
+                    .build()
+            }
+            return instance!!
+        }
+
+        // 문자열을 전송받을 시에 사용한다
+        private var instanceStr: Retrofit? = null
+        fun getInstanceStr(): Retrofit? {
+            if(instanceStr == null) {
+                val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+                StrictMode.setThreadPolicy(policy)
+
+                // ScalarsConverterFactory 만을 사용해야 한다
+                instanceStr = Retrofit.Builder()
+                    .baseUrl("http://70.12.113.169:3000/")
+                    .addConverterFactory(ScalarsConverterFactory.create())
+                    .build()
+            }
+            return instanceStr!!
+        }
+
+    }
+}
