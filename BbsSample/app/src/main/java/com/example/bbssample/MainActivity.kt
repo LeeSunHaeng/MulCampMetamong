1a1e5e15614bb9ad12992ee87c61df9ce23cf80d
PHaeBin <phb0721@gmail.com>
2022-03-17 09:21:10 +0900

    해빈 덮어쓰기용 커밋
    덮어쓰기

--- BbsSample/app/src/main/java/com/example/bbssample/MainActivity.kt
+++ BbsSample/app/src/main/java/com/example/bbssample/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         val loginBtn = findViewById<Button>(R.id.loginBtn)
         loginBtn.setOnClickListener {
 
-            val id = editId.text.toString()
+           /* val id = editId.text.toString()
             val password = editPw.text.toString()
             println("~~~~~~~~~~$id, $password")
             var dto = MemberDao.getInstance().login(MemberDto(id, password, "", "", 0))
@@ -26,13 +26,14 @@ class MainActivity : AppCompatActivity() {
                 MemberDao.user = dto
 
                 Toast.makeText(this, "${dto.name}님 환영합니다", Toast.LENGTH_LONG).show()
-
+*/
                 // login 되면 이동
-                val i = Intent(this, BbsListActivity::class.java)
+                //val i = Intent(this, BbsListActivity::class.java)
+                val i = Intent(this, Food_List_Meals::class.java)
                 startActivity(i)
-            }else {
-                Toast.makeText(this, "ID나 PW를 확인하세요", Toast.LENGTH_LONG).show()
-            }
+           // }else {
+           //     Toast.makeText(this, "ID나 PW를 확인하세요", Toast.LENGTH_LONG).show()
+           // }
         }
     }
 }
