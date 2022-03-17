d5a4eece9e4e62f713ee51dc284a3f45b33604b3
PHaeBin <phb0721@gmail.com>
2022-03-17 17:11:51 +0900

    0317박해빈17시커밋
    박해빈

--- BbsSample/app/src/main/java/com/example/bbssample/MainActivity.kt
+++ BbsSample/app/src/main/java/com/example/bbssample/MainActivity.kt
@@ -16,9 +16,10 @@ class MainActivity : AppCompatActivity() {
         val editPw = findViewById<EditText>(R.id.editPw)
         val nums = 10
         val loginBtn = findViewById<Button>(R.id.loginBtn)
+        val haebinBtn = findViewById<Button>(R.id.haebinBtn)
         loginBtn.setOnClickListener {
 
-           /* val id = editId.text.toString()
+            val id = editId.text.toString()
             val password = editPw.text.toString()
             println("~~~~~~~~~~$id, $password")
             var dto = MemberDao.getInstance().login(MemberDto(id, password, "", "", 0))
@@ -26,15 +27,18 @@ class MainActivity : AppCompatActivity() {
                 MemberDao.user = dto
 
                 Toast.makeText(this, "${dto.name}님 환영합니다", Toast.LENGTH_LONG).show()
-*/
-                // login 되면 이동
+
+                 //login 되면 이동
                 //val i = Intent(this, BbsListActivity::class.java)
-                val i = Intent(this, Food_List_Meals::class.java)
-                startActivity(i)
-           // }else {
-           //     Toast.makeText(this, "ID나 PW를 확인하세요", Toast.LENGTH_LONG).show()
-           // }
+
+            }else { Toast.makeText(this, "ID나 PW를 확인하세요", Toast.LENGTH_LONG).show()
+           }
         }
+
+        haebinBtn.setOnClickListener {
+            val i = Intent(this, Food_List_Meals::class.java)
+            startActivity(i)
+         }
     }
 }
 
