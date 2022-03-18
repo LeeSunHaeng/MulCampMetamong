318ef40e6734da5b43b5e59cdf72398c8869c1c2
Halo-oo <hihejyop@gmail.com>
2022-03-18 09:28:35 +0900

    [희정] 구독 회원정보 불러오기
    #21# 표시
    구독 회원정보 불러오기 기능 (현재 미완)

--- BbsSample/app/src/main/java/com/example/bbssample/MainActivity.kt
+++ BbsSample/app/src/main/java/com/example/bbssample/MainActivity.kt
@@ -3,39 +3,28 @@ package com.example.bbssample
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import java.lang.reflect.Member
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val editId = findViewById<EditText>(R.id.editId)
-        val editPw = findViewById<EditText>(R.id.editPw)
-        val nums = 10
         val loginBtn = findViewById<Button>(R.id.loginBtn)
+        // #21# Login 버튼 클릭 시 main button 페이지로 이동
         loginBtn.setOnClickListener {
 
-           /* val id = editId.text.toString()
-            val password = editPw.text.toString()
-            println("~~~~~~~~~~$id, $password")
-            var dto = MemberDao.getInstance().login(MemberDto(id, password, "", "", 0))
-            if(dto != null){
-                MemberDao.user = dto
+            /* #21# [for 구독여부 판단, test용] Login Button 클릭 시 현재 로그인한 사용자의 정보를 MemberSingleton에 저장 */
+            MemberSingleton.id = "zeze"
+            MemberSingleton.subscribe = "1"             // 1 = 구독
+            //MemberSingleton.subscribe = "0"          // 0 = 비구독
+            Log.d("MainActivity", "#21# 현재 로그인한 사용자의 정보(MemberSingleton) ${MemberSingleton.toString()}")
 
-                Toast.makeText(this, "${dto.name}님 환영합니다", Toast.LENGTH_LONG).show()
-*/
-                // login 되면 이동
-                //val i = Intent(this, BbsListActivity::class.java)
-                val i = Intent(this, Food_List_Meals::class.java)
-                startActivity(i)
-           // }else {
-           //     Toast.makeText(this, "ID나 PW를 확인하세요", Toast.LENGTH_LONG).show()
-           // }
+
+            val i = Intent(this,  MainButtonActivity::class.java)
+            startActivity(i)
         }
     }
 }
-
-// 안녕하세요.
