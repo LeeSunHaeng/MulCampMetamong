c0cb53bfa5097d38c050f8a966ce4ccbd133e9a4
LEESUNHANG2 <95415549+LEESUNHANG2@users.noreply.github.com>
2022-03-29 19:08:22 +0900

    MemberDto
    dto수정

--- BbsSample/app/src/main/java/com/metamom/bbssample/FoodListMeals/FoodListMeals.kt
+++ BbsSample/app/src/main/java/com/metamom/bbssample/FoodListMeals/FoodListMeals.kt
@@ -28,6 +28,8 @@ class FoodListMeals : AppCompatActivity() {
         var clickDay = findViewById<TextView>(R.id.clickDay)
         val myFoodListBtn = findViewById<TextView>(R.id.myFoodListBtn)
 
+        println(FoodList.get(0).seqfoodlist)
+
 
         cal.setOnDateChangeListener {cal,year,month,day ->
             clickDay.text = "$year-${month+1}-$day"
