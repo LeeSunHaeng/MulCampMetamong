fec29b62209103df2df9687b2b9d4a3594b815cc
Halo-oo <hihejyop@gmail.com>
2022-03-30 10:39:21 +0900

    [구독] v0.32
    git merge 성공 후 backup

--- BbsSample/app/src/main/java/com/metamom/bbssample/MainButtonActivity.kt
+++ BbsSample/app/src/main/java/com/metamom/bbssample/MainButtonActivity.kt
@@ -14,9 +14,6 @@ import com.metamom.bbssample.subscribe.*
 import com.metamom.bbssample.subsingleton.MemberSingleton
 import com.metamom.bbssample.subsingleton.SubTodayMealSingleton
 
-
-/* #21# [구독] */
-
 class MainButtonActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,7 +45,7 @@ class MainButtonActivity : AppCompatActivity() {
 
                     // 사용자 키/몸무게에 따른 하루 권장 칼로리 계산  == ((자신의 키 - 100) * 0.9) * 활동지수(30으로 고정)  &  !!소수점 2째자리까지만 포함(반올림X, 자르기o)
                     var totalKcal = String.format("%.2f", (((MemberSingleton.height!! - 100) * 0.9) * 30)).toDouble()
-                    Log.d("MainActivity", "#21# 하루 권장 칼로리 > $totalKcal")
+                    Log.d("MainButtonActivity", "#21# 하루 권장 칼로리 > $totalKcal")
 
                     // 하루 권장 칼로리를 아침, 점심, 저녁, 간식 칼로리 비율로 저장 (아 15%, 점 50%, 저 20%, 간 15%)
                     SubTodayMealSingleton.morningKcal = String.format("%.2f", totalKcal * 15 / 100).toDouble()
@@ -56,11 +53,11 @@ class MainButtonActivity : AppCompatActivity() {
                     SubTodayMealSingleton.dinnerKcal = String.format("%.2f", totalKcal * 20 / 100).toDouble()
                     SubTodayMealSingleton.snackKcal = String.format("%.2f", totalKcal * 15 / 100).toDouble()
 
-                    Log.d("MainActivity", "#21# 구독회원 정보 SubTodayMealSingleton 확인 > ${SubTodayMealSingleton.toString()}")
+                    Log.d("MainButtonActivity", "#21# 구독회원 정보 SubTodayMealSingleton 확인 > ${SubTodayMealSingleton.toString()}")
                 }
                 // 2-ii) SuccessEnd인 경우 == 구독만료
                 if (subEndCheck == "SuccessEnd"){
-                    Log.d("MainActivity", "#21# 구독 만료임에 따라 멤버 TABLE 구독값 수정 & 구독 TABLE 내 삭제")
+                    Log.d("MainButtonActivity", "#21# 구독 만료임에 따라 멤버 TABLE 구독값 수정 & 구독 TABLE 내 삭제")
 
                     /* !!!! 회원정보 다시 가져와서 MemberSingleton에 넣기 _구독만료로 구독값 변경했으니까 */
                     val userInfo = SnsDao.getInstance().snsGetMember(MemberSingleton.id.toString())
@@ -72,15 +69,18 @@ class MainButtonActivity : AppCompatActivity() {
                 }
             }
             else {
-                Log.d("MainActivity", "#21# 로그인 > 구독회원일 경우 > 구독정보 null Error 발생")
+                Log.d("MainButtonActivity", "#21# 로그인 > 구독회원일 경우 > 구독정보 null Error 발생")
             }
+        }
+        else if (MemberSingleton.subscribe == "0") {
+            Log.d("MainButtonActivity", "#21# 현재 로그인한 사용자는 구독회원이 아님")
         }
 
 
         /* #21# '구독 신청' Button 클릭 시 구독 여부 확인
         *   case_1) 구독일 경우 구독 정보 출력 페이지로 이동
         *   case_2) 구독이 아닐경우 구독 신청 페이지로 이동 */
-        val SubBtn = findViewById<Button>(R.id.main_subBtn)
+        /*val SubBtn = findViewById<Button>(R.id.main_subBtn)
         SubBtn.setOnClickListener {
 
             if (MemberSingleton.subscribe == "0") {          // case_2) 구독이 아닐 경우
@@ -91,10 +91,10 @@ class MainButtonActivity : AppCompatActivity() {
                 val i = Intent(this, SubInfoActivity::class.java)
                 startActivity(i)
             }
-        }
+        }*/
 
-        /* #21# 식단정보 Button 클릭 */
-        val MealBtn = findViewById<Button>(R.id.main_myMealsBtn)
+        /* #21# 식단정보 Button 클릭 > 오늘의 식단 */
+        /*val MealBtn = findViewById<Button>(R.id.main_myMealsBtn)
         MealBtn.setOnClickListener {
 
             if (MemberSingleton.subscribe == "0") {          // case_2) 구독이 아닐 경우
@@ -106,11 +106,11 @@ class MainButtonActivity : AppCompatActivity() {
                 val i = Intent(this, SubMyMealsActivity::class.java)
                 startActivity(i)
             }
-        }
+        }*/
 
 
 
-        val snsBtn = findViewById<Button>(R.id.SnsBtn)
+        /*val snsBtn = findViewById<Button>(R.id.SnsBtn)
         val haebinBtn = findViewById<Button>(R.id.haebinBtn)
 
         //sns 이동 버튼
@@ -136,6 +136,6 @@ class MainButtonActivity : AppCompatActivity() {
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent.addFlags(FLAG_ACTIVITY_CLEAR_TOP))
             }
-        }
+        }*/
     }
 }
